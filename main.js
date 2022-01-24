40447356f0ce900e2a9978ef89a134fce770fe2b
sabari245 <sabari2004@hotmail.com>
2022-01-24 12:02:38 +0530

    converted to hello world

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+// const http = require("http");
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  //   res.sendFile(__dirname + "/index.html");
+  res.send("Hello, World!");
 });
 
-io.on("connection", (socket) => {
-  socket.on("data", function (data) {
-    io.emit("chat", data);
-  });
-  console.log("a user connected");
-});
+// io.on("connection", (socket) => {
+//   socket.on("data", function (data) {
+//     io.emit("chat", data);
+//   });
+//   console.log("a user connected");
+// });
 
 server.listen(port, () => {
   console.log("listening on port: " + port);
